61b4246cd799beec64cd28f53e0435929af08fe8
Ahad Ali <linkonahad10@gmail.com>
2024-10-11 12:55:45 +0600

    change experience

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,9 +19,12 @@ const About: NextPage = () => {
                     I am Ahad Ali a <span>Software Engineer</span>
                   </h2>
                   <p>
-                    I hold a bachelor degree in Computer Science and Engineering and have been working since January 2023 at a
-                    multinational company based in Singapore. Proficient in software engineering, I am a quick learner who enjoys
-                    exploring new technologies. I contribute effectively to team projects and strive to stay ahead of industry trends.
+                    I hold a bachelor degree in Computer Science and Engineering and have been working since January
+                    2023 at a
+                    multinational company based in Singapore. Proficient in software engineering, I am a quick learner
+                    who enjoys
+                    exploring new technologies. I contribute effectively to team projects and strive to stay ahead of
+                    industry trends.
                   </p>
                 </div>
               </div>
@@ -79,6 +82,63 @@ const About: NextPage = () => {
                   </div>
                 </div>
               </div>
+              <div className={'row'}>
+                <div className="experience padd-15">
+                  <h3 className="title">Experience</h3>
+                  <div className="row">
+                    <div className="timeline-box padd-15">
+                      <div className="timeline shadow-dark">
+                        {/* Timeline Item */}
+                        <div className="timeline-item">
+                          <div className="circle-dot"/>
+                          <h4 className="timeline-title">Software Engineer
+                            <span className={'timeline-company'}> @ hSenid Mobile Solutions (Pvt) Ltd </span>
+                          </h4>
+                          <h6 className="timeline-date">
+                          <i className="fa fa-calendar"/> 1st April, 2024 to Present
+                          </h6>
+                          <p className="timeline-text">
+                            • Developed a middleware application to integrate TELECOM APIs with applications, enabling
+                            seamless data exchange and communication. <br></br>
+                            • Configured a monitoring tool to track and analyze application performance in real-time,
+                            enhancing system reliability. <br></br>
+                            • Enhanced the TPS of legacy applications by 45% through performance optimizations,
+                            resulting in faster request processing. <br></br>
+                            • Created a web UI for changing database configurations, reducing manual error risks and
+                            improving efficiency by 60%. <br></br>
+                            • Mentored interns during the development process and performed code reviews. <br></br>
+                          </p>
+                        </div>
+                        {/* Timeline Item end */}
+                        {/* Timeline Item */}
+                        <div className="timeline-item">
+                          <h4 className="timeline-title">Associate Software Engineer
+                            <span className={'timeline-company'}> @ hSenid Mobile Solutions (Pvt) Ltd </span>
+                          </h4>
+                          <div className="circle-dot"/>
+                          <h6 className="timeline-date">
+                            <i className="fa fa-calendar"/> 11 January, 2023 to 31 March 2024
+                          </h6>
+                          <p className="timeline-text">
+                            • Designed and developed a USSD application for M1 (Singapore), enabling users to seamlessly
+                            activate offers and transfer balances, which
+                            improved user engagement by 20%. <br/>
+                            • Developed a mobile banking solution for RawBank (Congo), supporting banking operations on
+                            all mobile phones, resulting in a 100% increase
+                            in customer engagement via USSD. <br/>
+                            • Upgraded application dependencies, reducing vulnerabilities and enhancing platform
+                            security and stability.
+                          </p>
+                        </div>
+                        {/* Timeline Item end */}
+                        {/* Timeline Item */}
+                        {/* Timeline Item end */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
               <div className="row">
                 <div className="education padd-15">
                   <h3 className="title">Education</h3>
@@ -97,62 +157,25 @@ const About: NextPage = () => {
                             Computer Science and Engineering
                           </h4>
                           <p className="timeline-text">
-                            From January, 2019 I started my journey to be a Software Engineer on Daffodil International University and finished March 2023.
+                            From January, 2019 I started my journey to be a Software Engineer on Daffodil International
+                            University and finished March 2023.
                           </p>
                         </div>
                         {/* Timeline Item end */}
                         {/* Timeline Item */}
                         <div className="timeline-item">
-                          <div className="circle-dot" />
+                          <div className="circle-dot"/>
                           <h6 className="timeline-date">
-                            <i className="fa fa-calendar" /> 2016 to 2018
+                            <i className="fa fa-calendar"/> 2016 to 2018
                           </h6>
                           <h4 className="timeline-title">
                             Higher Secondary Certificate
                           </h4>
                           <p className="timeline-text">
-                            In 2018 I passed HSC Exam from Science Group. My college name was Government Shahid Sohrawardi College.
+                            In 2018 I passed HSC Exam from Science Group. My college name was Government Shahid
+                            Sohrawardi College.
                           </p>
                         </div>
-                        {/* Timeline Item end */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="experience padd-15">
-                  <h3 className="title">Experience</h3>
-                  <div className="row">
-                    <div className="timeline-box padd-15">
-                      <div className="timeline shadow-dark">
-                        {/* Timeline Item */}
-                        <div className="timeline-item">
-                          <div className="circle-dot" />
-                          <h6 className="timeline-date">
-                            <i className="fa fa-calendar" /> 1st April, 2024 to Present
-                          </h6>
-                          <h4 className="timeline-title">Software Engineer </h4>
-                          <p className="timeline-text">
-                            From 2018 I am woking on Graphics Design. The
-                            software I use Photoshop, Illustrator for Graphics
-                            design.
-                          </p>
-                        </div>
-                        {/* Timeline Item end */}
-                        {/* Timeline Item */}
-                        <div className="timeline-item">
-                          <div className="circle-dot" />
-                          <h6 className="timeline-date">
-                            <i className="fa fa-calendar"/> 11 January, 2023 to 31 March 2024 at
-                          </h6>
-                          <h4 className="timeline-title">Associate Software Engineer</h4>
-                          <p className="timeline-text">
-                            Since 2020 I started to learn Coding. I have learn
-                            Html, Css, Sass, JavaScript, Bootstrap. Now I am a
-                            Professional Web Designer..
-                          </p>
-                        </div>
-                        {/* Timeline Item end */}
-                        {/* Timeline Item */}
                         {/* Timeline Item end */}
                       </div>
                     </div>
