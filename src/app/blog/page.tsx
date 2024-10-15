645b61264531c23da3f9803607ef62df1fa787cb
Ahad Ali <linkonahad10@gmail.com>
2024-10-15 16:01:56 +0600

    add all my medium blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,92 +1,72 @@
-import type {NextPage} from "next";
+'use client'
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
-const Blog: NextPage = () => {
+interface Article {
+    author: string;
+    categories: string[];
+    content: string;
+    description: string;
+    enclosure: Record<string, unknown>;
+    guid: string;
+    link: string;
+    pubDate: string;
+    thumbnail: string | null;
+    title: string;
+}
+
+const Blog: React.FC = () => {
+    const [articles, setArticles] = useState<Article[]>([]);
+
+    useEffect(() => {
+        fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@linkonahad10")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.items);
+                const items = data.items as Article[];
+                setArticles(items);
+            });
+    }, []);
+
     return (
-        <>
-            <section className="blog section" id="blog">
-                <div className="container">
-                    <div className="row">
-                        <div className="section-title padd-15">
-                            <h2>Latest Blog Posts</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        {/* Blog Item 1 */}
-                        <div className="blog-item padd-15">
-                            <div className="blog-item-inner shadow-dark">
-                                <div className="blog-img">
-                                    <img src="/imgs/blog/1.jpg" alt="Responsive Web Design"/>
-                                    <div className="blog-date">June 4, 2020</div>
-                                </div>
-                                <div className="blog-info">
-                                    <h4 className="blog-title">
-                                        Mastering Responsive Web Design
-                                    </h4>
-                                    <p className="blog-description">
-                                        In today digital landscape, responsive web design is not
-                                        just a trend; it is a necessity. Explore the principles and
-                                        techniques behind creating websites that adapt seamlessly to
-                                        various devices and screen sizes.
-                                    </p>
-                                    <p className="blog-tags">
-                                        Tags: <a href="#">HTML</a>, <a href="#">CSS</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Blog Item 1 End */}
-                        {/* Blog Item 2 */}
-                        <div className="blog-item padd-15">
-                            <div className="blog-item-inner shadow-dark">
-                                <div className="blog-img">
-                                    <img src="/imgs/blog/2.jpg" alt="Creative Slideshow"/>
-                                    <div className="blog-date">June 4, 2020</div>
-                                </div>
-                                <div className="blog-info">
-                                    <h4 className="blog-title">
-                                        Unleashing Creativity with Slideshows
-                                    </h4>
-                                    <p className="blog-description">
-                                        Discover innovative ways to create captivating slideshows
-                                        that engage your audience. From stunning visuals to seamless
-                                        transitions, this blog post explores the art of crafting
-                                        memorable slideshows for your website.
-                                    </p>
-                                    <p className="blog-tags">
-                                        Tags: <a href="#">HTML</a>, <a href="#">CSS</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Blog Item 2 End */}
-                        {/* Blog Item 3 */}
-                        <div className="blog-item padd-15">
-                            <div className="blog-item-inner shadow-dark">
-                                <div className="blog-img">
-                                    <img src="/imgs/blog/3.jpg" alt="Image Gallery Lightbox"/>
-                                    <div className="blog-date">June 4, 2020</div>
-                                </div>
-                                <div className="blog-info">
-                                    <h4 className="blog-title">
-                                        Enhancing User Experience with Image Gallery Lightboxes
-                                    </h4>
-                                    <p className="blog-description">
-                                        Explore the benefits of incorporating image gallery
-                                        lightboxes into your website design. From improved user
-                                        experience to showcasing your visual content in style, this
-                                        blog post sheds light on this valuable web design feature.
-                                    </p>
-                                    <p className="blog-tags">
-                                        Tags: <a href="#">HTML</a>, <a href="#">CSS</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        {/* Blog Item 3 End */}
+        <section className="blog section" id="blog">
+            <div className="container">
+                <div className="row">
+                    <div className="section-title padd-15">
+                        <h2>Latest Blog Posts</h2>
                     </div>
                 </div>
-            </section>
-        </>
+                <div className="row">
+                    {articles.map((article, index) => (
+                        <div className="blog-item padd-15" key={index}>
+                            <div className="blog-item-inner shadow-dark">
+                                <div className="blog-img">
+                                    <img
+                                        src={(article.description as any).toString().match(/<img[^>]+src="([^">]+)"/)?.[1] || '/imgs/blog/default.jpg'}
+                                        alt={article.title}
+                                        className="blog-image" // Apply the responsive class here
+                                    />
+                                    <div className="blog-date">{new Date(article.pubDate).toDateString()}</div>
+                                </div>
+                                <div className="blog-info">
+                                    <h4 className="blog-title">{article.title}</h4>
+                                    <p className="blog-description">{article.description}</p>
+                                    <p className="blog-tags">
+                                        Tags: {article.categories.map((category, index) => (
+                                        <a href={`#${category}`} key={index}>
+                                            {category}
+                                            {index < article.categories.length - 1 && ', '}
+                                        </a>
+                                    ))}
+                                    </p>
+                                    <Link href={article.link} className="blog-link"> Read More </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 };
 
