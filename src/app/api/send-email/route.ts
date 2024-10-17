4a3925d1f1bd3b507adc377e73303414e96c9ead
Ahad Ali <linkonahad10@gmail.com>
2024-10-17 15:57:52 +0600

    change email body from plaintext to html

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,14 +1,31 @@
-
 import nodemailer from 'nodemailer';
-import {NextRequest, NextResponse} from "next/server";
-import {EmailBody} from "@/app/type/EmailBody";
+import { NextRequest, NextResponse } from "next/server";
+import { EmailBody } from "@/app/type/EmailBody";
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export async function POST(req: NextRequest) {
-    const {name, email, subject, message} = await req.json() as EmailBody;
+    const { name, email, subject, message } = await req.json() as EmailBody;
 
     if (!name || !subject || !email || !message) {
-        return NextResponse.json({ error: 'Missing required fields: to, subject, body' }, { status: 400 });
+        return NextResponse.json({ error: 'Missing required fields: name, email, subject, message' }, { status: 400 });
     }
+
+    const templatePath = path.join(process.cwd(), 'public', 'html', 'email-template.html');
+
+    let emailHtml;
+    try {
+        emailHtml = await fs.readFile(templatePath, 'utf-8');
+    } catch (err) {
+        console.error("Failed to read the HTML template:", err);
+        return NextResponse.json({ message: 'Failed to load email template' }, { status: 500 });
+    }
+
+    emailHtml = emailHtml
+        .replace('{{name}}', name)
+        .replace('{{email}}', email)
+        .replace('{{subject}}', subject)
+        .replace('{{message}}', message);
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -18,18 +35,16 @@ export async function POST(req: NextRequest) {
         },
     });
     const mailOptions = {
-        from: process.env.GMAIL_USERNAME,
+        from: process.env.GMAIL_HOST_USERNAME,
         to: process.env.GMAIL_RECEIVER_EMAIL,
         subject: subject,
-        text: message,
+        html: emailHtml,
     };
 
     try {
         const info = await transporter.sendMail(mailOptions);
-        return NextResponse.json({ message: 'Email sent successfully'}, { status: 200 });
+        return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
     } catch (error) {
-        console.log(error);
-        return NextResponse.json({ message: 'Email sent failed'}, { status: 500 });
+        return NextResponse.json({ message: 'Failed to send email' }, { status: 500 });
     }
 }
-
