9a82922ea0b1a43c0681090df4aaff85b24bd281
Ahad Ali <linkonahad10@gmail.com>
2025-04-26 11:24:22 +0600

    resolve path issues

--- src/components/body/Skills.tsx
+++ src/components/body/Skills.tsx
@@ -9,17 +9,17 @@ type Skill = {
 };
 
 const skillsList: Skill[] = [
-    { name: "Java", path: "/imgs/skills/java.png", width: 120, height: 120 },
-    { name: "Kotlin", path: "/imgs/skills/kotlin.png", width: 100, height: 100 },
-    { name: "Spring Framework", path: "/imgs/skills/spring-framework.png", width: 200, height: 100 },
-    { name: "Spring Boot", path: "/imgs/skills/spring-boot.png", width: 200, height: 100 },
-    { name: "JAX-WS", path: "/imgs/skills/jax-ws.png", width: 200, height: 100 },
-    { name: "Netty", path: "/imgs/skills/netty.png", width: 200, height: 100 },
-    { name: "Hibernate", path: "/imgs/skills/hibernate.png", width: 200, height: 100 },
-    { name: "Hibernate", path: "/imgs/skills/type-script.png", width: 100, height: 100 },
-    { name: "Hibernate", path: "/imgs/skills/next.png", width: 100, height: 100 },
-    { name: "Hibernate", path: "/imgs/skills/mysql.png", width: 100, height: 100 },
-    { name: "Hibernate", path: "/imgs/skills/docker.png", width: 100, height: 100 },
+    { name: "Java", path: "./imgs/skills/java.png", width: 120, height: 120 },
+    { name: "Kotlin", path: "./imgs/skills/kotlin.png", width: 100, height: 100 },
+    { name: "Spring Framework", path: "./imgs/skills/spring-framework.png", width: 200, height: 100 },
+    { name: "Spring Boot", path: "./imgs/skills/spring-boot.png", width: 200, height: 100 },
+    { name: "JAX-WS", path: "./imgs/skills/jax-ws.png", width: 200, height: 100 },
+    { name: "Netty", path: "./imgs/skills/netty.png", width: 200, height: 100 },
+    { name: "Hibernate", path: "./imgs/skills/hibernate.png", width: 200, height: 100 },
+    { name: "Hibernate", path: "./imgs/skills/type-script.png", width: 100, height: 100 },
+    { name: "Hibernate", path: "./imgs/skills/next.png", width: 100, height: 100 },
+    { name: "Hibernate", path: "./imgs/skills/mysql.png", width: 100, height: 100 },
+    { name: "Hibernate", path: "./imgs/skills/docker.png", width: 100, height: 100 },
 ];
 
 const Skills: React.FC = () => {
@@ -28,7 +28,7 @@ const Skills: React.FC = () => {
             <h3 className="title">Skills</h3>
             <div className="skills-row">
                 {skillsList.map((skill) => (
-                    <Image
+                    <img
                         key={skill.name}
                         className="skill-image"
                         src={skill.path}
