4d426f62c9f9412cc8e7065b7d76b5d4147676b1
md-ahad-ali <linkonahad10@gmail.com>
2025-05-01 19:23:56 +0600

    change resume

--- src/components/body/PdfViewer.tsx
+++ src/components/body/PdfViewer.tsx
@@ -4,7 +4,7 @@ const PdfViewer: React.FC = () => {
     return (
         <div style={{ width: '100%', height: '100vh' }}>
             <iframe
-                src="./assets/docs/resume.pdf"
+                src="./assets/docs/Md.-Ahad-Ali-Resume-2025.pdf"
                 style={{ width: '100%', height: '100%' }}
                 title="Resume PDF"
             />
